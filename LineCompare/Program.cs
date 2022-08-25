8b856755632f0063b099681a33bac8d02ce08ff8
Darsh <darshankondhare2698@gmail.com>
2022-08-25 16:29:49 +0530

    [darsh]-UC1 LineComparision

--- LineCompare/Program.cs
+++ LineCompare/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //UC1 uc = new UC1();
+            //uc.Length();
         }
     }
 }
